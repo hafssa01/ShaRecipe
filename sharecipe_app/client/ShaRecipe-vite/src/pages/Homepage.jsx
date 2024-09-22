7c6e227b937db9df2f063911a07dad516cbc61cf
boost1ne <boustane.chaimaa@gmail.com>
2024-09-22 20:24:15 +0100

    landing page update

--- sharecipe_app/client/ShaRecipe-vite/src/pages/Homepage.jsx
+++ sharecipe_app/client/ShaRecipe-vite/src/pages/Homepage.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import NavBar from '../components/Navscroll';
 import Footer from '../components/Footer';
+import CarouselComponent from '../components/carousel';
+import CardComponent from '../components/cards';
 
 function Home() {
   return (
     <div>
       <NavBar />
-      <p>Hello visitor</p>
+
+      <CarouselComponent />
+      <CardComponent />
+
       <Footer/>
       
  
